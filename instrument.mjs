ea57ac059f097e3e13ab7027fcccfa69e3154938
Hernán Rago <hernanrago@msn.com>
2024-05-20 23:57:10 -0300

    Update instrument.mjs

--- instrument.mjs
+++ instrument.mjs
@@ -3,7 +3,12 @@ export async function getInstrument(ticker, date) {
   const url = `https://iol.invertironline.com/api/cotizaciones/history?symbolName=${ticker}&exchange=BCBA&from=${Math.floor(date.getTime() / 1000)}&to=${Math.floor(endDate.getTime() / 1000)}&resolution=D`;
 
   try {
-    const response = await fetch(url, { mode: 'cors' });
+    const response = await fetch(url, { 
+      mode: 'cors',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+    })});
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
